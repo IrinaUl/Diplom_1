a2a1fc1c9683ede6ab5b3311c87b97307a906d2e
Ирина Попкова <uljana@Irinas-MacBook-Air.local>
2024-11-17 18:51:14 +0100

    add IngredientTests

--- src/test/java/praktikum/BunTests.java
+++ src/test/java/praktikum/BunTests.java
@@ -1,6 +1,7 @@
 package praktikum;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -12,8 +13,9 @@ import java.util.Collection;
 public class BunTests {
     private final String name;
     private final float price;
+    private Bun bun;
 
-    public BunTests (String name, float price) {
+    public BunTests(String name, float price) {
         this.name = name;
         this.price = price;
     }
@@ -24,22 +26,27 @@ public class BunTests {
                 {"black bun", 100},
                 {"white bun", 200},
                 {"red bun", 300},
-                {"test", -5.5},
+                {"test", -5},
                 {"test", 0},
+                {"test", 5.5f},
+                {"", -5.5f},
                 {"", 0},
                 {null, 0},
         });
     }
 
+    @Before
+    public void startUp() {
+        bun = new Bun(name, price);
+    }
+
     @Test
     public void checkNameBun() {
-        Bun bun = new Bun(name, price);
         Assert.assertEquals("Ошибка названия булочки", name, bun.getName());
     }
 
     @Test
     public void checkPriceBun() {
-        Bun bun = new Bun(name, price);
         Assert.assertEquals("Ошибка цены булочки", price, bun.getPrice(), 0);
     }
 }
--- src/test/java/praktikum/BurgerTests.java
+++ src/test/java/praktikum/BurgerTests.java
@@ -1,4 +1,66 @@
 package praktikum;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class BurgerTests {
+    Burger burger;
+
+    @Mock
+    private Bun bun;
+
+    @Mock
+    private Ingredient ingredient;
+
+    @Mock
+    private Ingredient ingredientFirst;
+
+    @Mock
+    private Ingredient ingredientSecond;
+
+    @Before
+    public void startUp() {
+        burger = new Burger();
+    }
+
+    @Test
+    public void checkAddIngredient() {
+        int numIngredients = burger.ingredients.size();
+        burger.addIngredient(ingredient);
+        Assert.assertEquals("Количество ингредиентов не совпадает", numIngredients + 1, burger.ingredients.size());
+    }
+
+    @Test
+    public void checkRemoveIngredient() {
+        burger.addIngredient(ingredient);
+        burger.addIngredient(ingredient);
+        int numIngredients = burger.ingredients.size();
+        burger.removeIngredient(0);
+        Assert.assertEquals("Количество ингредиентов не совпадает", numIngredients - 1, burger.ingredients.size());
+    }
+
+    @Test
+    public void checkMoveIngredient() {
+        burger.addIngredient(ingredient);
+        burger.addIngredient(ingredientFirst);
+        burger.addIngredient(ingredientSecond);
+        burger.moveIngredient(2, 0);
+        Assert.assertEquals("Ингредиент не на своем месте", 0, burger.ingredients.indexOf(ingredientSecond));
+    }
+
+    //todo Дописать метод
+    @Test
+    public void checkPriceBurger() {
+        burger.addIngredient(ingredient);
+        burger.addIngredient(ingredientFirst);
+
+        Assert.assertEquals("Цена должна совпадать", , );
+    }
+
+
 }
